a9432b1ecb2af1c9abfe804a968d6503cc77bdbb
Av32000 <ae.av32000@gmail.com>
2024-12-08 12:54:23 +0100

    Day 8

---
+++ 08/part2.js
@@ -0,0 +1,67 @@
+const { readFileSync } = require("fs");
+// Parse Input
+const inputFile = "input.txt";
+const input = readFileSync(inputFile).toString();
+const grid = [];
+input
+  .trim()
+  .split("\n")
+  .forEach((line) => grid.push(line.split("")));
+
+let antenaPositions = [];
+
+for (let y = 0; y < grid.length; y++) {
+  const line = grid[y];
+  for (let x = 0; x < line.length; x++) {
+    const element = line[x];
+    if (element != ".") {
+      antenaPositions.push({
+        id: antenaPositions.length,
+        type: element,
+        x,
+        y,
+      });
+    }
+
+    grid[y][x] = element;
+  }
+}
+
+function isInGrid(x, y) {
+  return x >= 0 && y >= 0 && x < grid[0].length && y < grid.length;
+}
+
+const antinodes = [...antenaPositions];
+antenaPositions.forEach((a) => {
+  antenaPositions
+    .filter((ca) => a.type == ca.type && ca.id != a.id)
+    .forEach((o) => {
+      let xDiff = o.x - a.x;
+      let yDiff = o.y - a.y;
+      let antinode = { type: a.type, x: a.x - xDiff, y: a.y - yDiff };
+
+      if (xDiff == 0 && yDiff == 0) {
+        antinodes.push(antinode);
+      } else {
+        while (true) {
+          if (isInGrid(antinode.x, antinode.y)) {
+            if (
+              !antinodes.find((n) => n.x == antinode.x && n.y == antinode.y)
+            ) {
+              antinodes.push(antinode);
+            }
+
+            antinode = {
+              type: a.type,
+              x: antinode.x - xDiff,
+              y: antinode.y - yDiff,
+            };
+          } else {
+            break;
+          }
+        }
+      }
+    });
+});
+
+console.log(antinodes.length);
